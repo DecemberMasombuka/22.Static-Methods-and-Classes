992c518fee73f1c9f84fadf586b82475e6b64cde
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 16:11:57 +0200

    Add project files.

---
+++ UseFullTools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Methods_and_Classes
+{
+    static internal class UseFullTools
+    {
+        
+
+        public static void Greet(String name)
+        {
+            Console.WriteLine("Hello "+ name);
+        }
+    }
+}
